e7e0565d01f0f827fbd28c6e4656f2e21c18a8d5
Alan Klinger <klingerkrieg@gmail.com>
2021-12-16 21:14:52 -0300

    contato está enviando e-mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Notifications\NewContact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class ContactController extends Controller
 {
@@ -13,6 +15,12 @@ class ContactController extends Controller
 
     public function send(Request $request){
         $contact = Contact::create($request->all());
+
+        #envia o e-mail
+        Notification::route("mail", config("mail.from.address"))
+                ->notify(new NewContact($contact));
+
+
         return redirect()->back();
     }
 
